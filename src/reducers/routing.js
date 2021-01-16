0a4ee728d12ed1c64be49ce43a2407ec495a300f
artyom-f <etn019@mail.ru>
2021-01-16 13:17:47 +0300

    routing after deleting

--- src/reducers/routing.js
+++ src/reducers/routing.js
@@ -1,5 +1,7 @@
 import { SELECT_PAGE } from '../actions/routing';
-import { MAIN_PAGE } from '../constants/pages';
+import { MAIN_PAGE, BOOKS_PAGE, AUTHORS_PAGE } from '../constants/pages';
+import { DELETE_BOOK_SUCCESS } from '../actions/books';
+import { DELETE_AUTHOR_SUCCESS } from '../actions/authors';
 
 const initialStore = {
     activePage: MAIN_PAGE,
@@ -12,6 +14,16 @@ export default function routingReducer(store = initialStore, action) {
             return {
                 activePage: action.activePage,
                 entityId: action.entityId,
+            };
+        case DELETE_BOOK_SUCCESS:
+            return {
+                activePage: BOOKS_PAGE,
+                entityId: null,
+            };
+        case DELETE_AUTHOR_SUCCESS:
+            return {
+                activePage: AUTHORS_PAGE,
+                entityId: null,
             };
         default:
             return store;
